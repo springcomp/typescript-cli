7a2e6120a5b71f43a6190ded8677ec9fd056fb9d
Springcomp <springcomp@users.noreply.github.com>
2021-12-10 16:57:51 +0100

    Supports multiple commands.

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ https://medium.com/rubber-ducking/creating-a-cli-with-typescript-1c5112ae101f
 http://yargs.js.org/
 https://github.com/yargs/yargs/blob/main/docs/typescript.md
 https://markoskon.com/yargs-examples/
+
+### Multiple commands
+
+https://github.com/yargs/yargs/blob/main/docs/advanced.md#command-execution
--- cli.ts
+++ cli.ts
@@ -6,15 +6,36 @@ import yargs from 'yargs/yargs';
 const options: string[] = ["one", "two", "three"];
 type Options = typeof options[number];
 
-const parser = yargs(process.argv.slice(2)).options({
-  a: { type: 'boolean', default: false },
-  b: { type: 'string', demandOption: true },
-  c: { type: 'number', alias: 'chill' },
-  d: { type: 'array' },
-  e: { type: 'count' },
-  f: { choices: ['1', '2', '3'] },
-  o: { choices: options }
-})
+const parser = yargs(process.argv.slice(2))
+  .command({
+    command: "apple",
+    describe: "Apples, part of apples and oranges.",
+    builder: {
+      a: { type: 'boolean', default: false },
+      b: { type: 'string', demandOption: true },
+      c: { type: 'number', alias: 'chill' },
+      d: { type: 'array' },
+      e: { type: 'count' },
+      f: { choices: ['1', '2', '3'] },
+      o: { choices: options }
+    },
+    handler: (argv) => { console.log("apple"); }
+  })
+  .command({
+    command: "orange",
+    describe: "Oranges, part of apples and oranges.",
+    builder: {
+      a: { type: 'boolean', default: false },
+      b: { type: 'string', demandOption: true },
+      c: { type: 'number', alias: 'chill' },
+      d: { type: 'array' },
+      e: { type: 'count' },
+      f: { choices: ['1', '2', '3'] },
+      o: { choices: options }
+    },
+    handler: (argv) => { console.log("apple"); }
+  })
+  .demandCommand()
   .fail(function (msg, err) {
     console.log(msg);
     process.exit(0);
