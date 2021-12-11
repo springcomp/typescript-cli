91a133c4a08551cb2cf45ce40b461931cf7ac203
springcomp <springcomp@users.noreply.github.com>
2021-12-11 11:37:39 +0100

    MMoved declarations to module.

--- apple.ts
+++ apple.ts
@@ -1,12 +1,21 @@
+import { Arguments, InferredOptionTypes, Options } from "yargs";
 
-namespace apple {
+export namespace apple {
 
 	export const command: string = "apple";
 	export const describe: string = "Apples, part of apples and oranges.";
 
-	// TODO: find the type of handler
-	export handler = (argv) => {
-		console.log("apple");
-	}
+	export const builder: any = {
+      a: { type: 'boolean', default: false },
+      b: { type: 'string', demandOption: true },
+      c: { type: 'number', alias: 'chill' },
+      d: { type: 'array' },
+      e: { type: 'count' },
+      f: { choices: ['1', '2', '3'] },
+    };
 
+	export const handler: (args: Arguments<InferredOptionTypes<{ [key: string]: Options }>>) => void | Promise<void> =
+		(argv: any) => {
+			console.log("apple");
+		};
 }
--- cli.ts
+++ cli.ts
@@ -1,23 +1,16 @@
 #!/usr/bin/env node
 
+import { appendFile } from 'fs';
 import { argv } from 'process';
 import yargs from 'yargs/yargs';
+import { apple } from './apple';
 
 const parser = yargs(process.argv.slice(2))
   .command({
-    command: "apple",
-    describe: "Apples, part of apples and oranges.",
-    // TODO: find the type of builder
-    builder: {
-      a: { type: 'boolean', default: false },
-      b: { type: 'string', demandOption: true },
-      c: { type: 'number', alias: 'chill' },
-      d: { type: 'array' },
-      e: { type: 'count' },
-      f: { choices: ['1', '2', '3'] },
-    },
-    // TODO: find the type of handler
-    handler: (argv) => { console.log("apple"); }
+    command: apple.command,
+    describe: apple.describe,
+    builder: apple.builder, 
+    handler: apple.handler,
   })
   .command({
     command: "orange",
